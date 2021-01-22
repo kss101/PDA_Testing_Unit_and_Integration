876e61df290654902a5d0636a27d2460db04be92
Kurt S Steffan <kurt.s.steffan@gmail.com>
2021-01-22 18:25:18 +0000

    Added Integration tests 1-4

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,6 +16,44 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should update the result of the operation when using the arithmetical operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
 
-  
+  it('should allow multiple operations to be chained together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
+  it('should allow for different types of number in the calculation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click(); // to give a decimal value of 2.5
+
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click(); // to give a negative value of -1.5
+
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    var i;
+    for (i=0; i <=10; i++){
+      element(by.css('#number0')).click();} // to input 11 zeros after the 1
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('99999999999.5')
+  })
+
 });
