7febf116e2b8308f909dbe64381318eda6f437d4
Kurt S Steffan <kurt.s.steffan@gmail.com>
2021-03-01 18:41:35 +0000

    Amended test 4 to be three seperate tests: for a decimal, a negative, and for a very large number

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const { element } = require('protractor');
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
@@ -36,24 +37,34 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4')
   })
 
-  it('should allow for different types of number in the calculation', function(){
+  it('should allow for different types of number in the calculation: positive decimal', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number5')).click();
     element(by.css('#operator_divide')).click();
     element(by.css('#number2')).click();
     element(by.css('#operator_equals')).click(); // to give a decimal value of 2.5
+    expect(running_total.getAttribute('value')).to.eventually.equal('2.5')
+  })
 
+  it('should allow for different types of number in the calculation: negative value', function(){  
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
     element(by.css('#operator_subtract')).click();
     element(by.css('#number3')).click();
-    element(by.css('#operator_equals')).click(); // to give a negative value of -1.5
+    element(by.css('#operator_equals')).click(); // to give a negative value of -0.5
+    expect(running_total.getAttribute('value')).to.eventually.equal('-2')
+  })
 
+  it('should allow for different types of number in the calculation: very large value', function(){ 
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
     element(by.css('#operator_add')).click();
     element(by.css('#number1')).click();
     var i;
-    for (i=0; i <=10; i++){
-      element(by.css('#number0')).click();} // to input 11 zeros after the 1
+    for (i=0; i <=20; i++){
+      element(by.css('#number0')).click();} // to input 20 zeros after the 1
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('99999999999.5')
+    expect(running_total.getAttribute('value')).to.eventually.equal('1e+21') //display swaps to scientific notation
   })
 
   it('should display Error on division by zero', function(){
